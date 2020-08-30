b990c162c729763d02a53d7713bb6449bd601faf
Semyon-Ivakaev <stixonforpython@gmail.com>
2020-08-30 17:37:50 +0300

    add login and design

---
+++ Login and design/app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/*
+В fields.xml цвета полей
+В button.xml цвета кнопки
+В active_main.xml все размеры
+ */
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
